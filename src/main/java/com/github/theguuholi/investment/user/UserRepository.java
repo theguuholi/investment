15b0cad7b42e7a5e1744259d3df10f2b7ed3fe82
Gustavo Oliveira <g.92oliveira@gmail.com>
2026-04-13 07:20:28 -0300

    feat: add UserDetailsService, auth DTOs, and findByEmail

--- src/main/java/com/github/theguuholi/investment/user/UserRepository.java
+++ src/main/java/com/github/theguuholi/investment/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.github.theguuholi.investment.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
